1c936261a8ea795a940ccd8a6fd142d45df44818
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2019-05-20 19:19:40 +0200

    Remove margin/padding from HTML boilerplate

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <style dangerouslySetInnerHTML={{__html: "html, body { margin: 0; padding: 0; }"}} />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
