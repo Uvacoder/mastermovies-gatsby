a0894dbfc8503b8fc2e1d8d17c45245966022686
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2019-05-20 19:24:47 +0200

    Add film card component

---
+++ src/components/common/film_card/film_card_resolver.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent, useState, useEffect } from "react";
+import { Overwrite, Subtract } from "utility-types";
+
+import { IFilmCardProps, FilmCard } from "./film_card";
+import { getFilm, IGlacierFilm, createCancelToken } from "../api/glacier";
+
+type reducedFilmCardProps = Subtract<IFilmCardProps, { film?: IGlacierFilm }>;
+interface IFilmCardResolverProps extends reducedFilmCardProps {
+  /** The fingerprint of the film */
+  film: string;
+}
+
+/**
+ * A smart component that will attempt to resolve a film from it's fingerprint, while passing
+ * down all original props, such as error and retry. If an internal error occurs, the error
+ * props will be overwritten to reflect the internal retry functions.
+ */
+export const FilmCardResolver: FunctionComponent<Overwrite<IFilmCardProps, IFilmCardResolverProps>> = ({ film, error, onRetry, ...rest }) => {
+
+  const [ resolvedFilm, setResolvedFilm ] = useState<IGlacierFilm>(null);
+  const [ resolveError, setResolveError ] = useState<boolean>(false);
+
+  useEffect(() => {
+
+    if (!error && film) {
+      const cancelToken = createCancelToken();
+
+      getFilm(cancelToken.token, film)
+        .then(setResolvedFilm)
+        .catch(() => {
+          setResolveError(true);
+        });
+
+      return cancelToken.cancel;
+    }
+
+  }, [film, error]);
+
+  return (
+    <FilmCard {...rest} error={resolveError? resolveError : error} onRetry={resolveError? () => setResolveError(false) : onRetry} film={resolvedFilm} />
+  );
+
+}
---
+++ src/components/common/film_card/film_card_thumbnail.tsx
@@ -0,0 +1,48 @@
+import Img, { FluidObject } from "gatsby-image";
+import React, { FunctionComponent, useState, useEffect } from "react";
+
+import { IGlacierFilmThumbnail } from "../api/glacier";
+import { generateFluid } from "./generate_fluid";
+
+interface IFilmCardImageProps {
+  thumbnails: IGlacierFilmThumbnail[];
+  className?: string;
+}
+
+export const FilmCardImage: FunctionComponent<IFilmCardImageProps> = ({ thumbnails, className }) => {
+
+  const [ fluid, setFluid ] = useState<FluidObject>(null);
+  const [ aspectRatio, setAspectRatio ] = useState(1);
+
+  useEffect(() => {
+    if (Array.isArray(thumbnails) && thumbnails.length > 0) {
+      let isSubscribed = true;
+      const { cancel, promise, aspectRatio } = generateFluid(thumbnails);
+      setAspectRatio(aspectRatio);
+
+      promise.then(data => {
+        if (isSubscribed) {
+          setFluid(data);
+        }
+      })
+
+      return () => {
+        isSubscribed = false;
+        cancel();
+      }
+
+    }
+  }, thumbnails);
+
+  const resolvedFluid = fluid? fluid : {
+    aspectRatio,
+    src: "",
+    srcSet: "",
+    srcWebp: "",
+    srcSetWebp: "",
+    sizes: ""
+  };
+
+  return <Img className={className} fluid={resolvedFluid} />;
+
+}
