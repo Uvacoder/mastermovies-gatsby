0c1a6a151dc171b0bbe1f413b7d4d1264c607521
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2019-06-08 19:27:32 +0200

    feat(component): Modal blocks scrolling, improve mobile friendliness of components

--- src/components/common/modal/modal.tsx
+++ src/components/common/modal/modal.tsx
@@ -1,6 +1,6 @@
 import { PageHeader } from "antd";
 import classnames from "classnames";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import { Transition, TransitionGroup } from "react-transition-group";
 
 import { Spinner } from "../spinner";
@@ -31,6 +31,31 @@ export const Modal: FunctionComponent<IModalProps> = ({
   onBack = () => {},
   children
 }) => {
+
+  // Lock the body from scrolling, without changing the viewport size, using a shared queue with other modals
+  useEffect(() => {
+    if (active) {
+      const body = document.getElementsByTagName("body")[0];
+      const locks = parseInt(body.getAttribute("data-modal-lock")) || 0;
+      const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+
+      body.style.overflow = "hidden";
+      body.style.paddingRight = Math.round(scrollbarWidth).toString() + "px";
+
+      body.setAttribute("data-modal-lock", (locks + 1).toString());
+
+      return () => {
+        const newLocks = parseInt(body.getAttribute("data-modal-lock")) || 0;
+        if (newLocks <= 1) {
+          body.style.overflow = "";
+          body.style.paddingRight = "";
+          body.removeAttribute("data-modal-lock");
+        } else {
+          body.setAttribute("data-modal-lock", (newLocks - 1).toString());
+        }
+      }
+    }
+  }, [ active ]);
 
   // Find last valid child element
   let lastIndex = 0;
