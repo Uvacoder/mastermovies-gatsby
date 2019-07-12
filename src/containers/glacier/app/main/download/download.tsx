d10459422f22088625948990a97fa13d43cc4086
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2019-07-12 12:02:37 +0200

    fix(component): Glacier download no longer resets scroll position

--- src/containers/glacier/app/main/download/download.tsx
+++ src/containers/glacier/app/main/download/download.tsx
@@ -1,13 +1,13 @@
+import { navigate } from "gatsby";
 import React, { FunctionComponent, useEffect, useState } from "react";
+import Helmet from "react-helmet";
 
 import { createCancelToken } from "../../../../../api/common";
-import { getFilm, IGlacierFilm, IGlacierFilmExport } from "../../../../../api/glacier";
+import { getFilm, IGlacierFilm } from "../../../../../api/glacier";
 import { Modal } from "../../../../../components/common/modal/modal";
+import { StandardOverlay } from "../../../../../components/common/standard_overlay";
 import { GlacierDownloadOverview } from "./overview/overview";
 import { GlacierDownloadWizard } from "./wizard";
-import Helmet from "react-helmet";
-import { navigate } from "gatsby";
-import { StandardOverlay } from "../../../../../components/common/standard_overlay";
 
 interface IGlacierDownloadProps {
   film?: string;
@@ -67,7 +67,7 @@ export const GlacierDownload: FunctionComponent<IGlacierDownloadProps> = ({ film
 
       <Modal
         active={!!film}
-        onBack={(all) => { all || !filmExport? onBack() : navigate("/glacier/film/" + film) }}
+        onBack={(all) => { all || !filmExport? onBack() : navigate("/glacier/film/" + film, {state: {noScroll: true}}) }}
         backText={filmExport? "Back to overview" : "Back to Glacier"}
       >
 
@@ -75,7 +75,7 @@ export const GlacierDownload: FunctionComponent<IGlacierDownloadProps> = ({ film
           <GlacierDownloadOverview
             key={`glacier-overview-${resolvedFilm.fingerprint}`}
             film={resolvedFilm}
-            onDownload={(filmExport => navigate(`/glacier/film/${film}/download/${filmExport.fingerprint}`))}
+            onDownload={(filmExport => navigate(`/glacier/film/${film}/download/${filmExport.fingerprint}`, {state: {noScroll: true}}))}
           />
         )}
 
