be3aced0ef9417f3d23ee54b3eb572985ae308ac
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2020-02-03 16:21:17 +0100

    build: hide mini-css-extract-plugin build warning
    CSS Modules are an alternative to correct CSS ordering

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,17 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  // Disable mini-css-extract-plugin's order warnings
+  // CSS Scoping using CSS Modules doesn't need ordering
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
